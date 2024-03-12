2335a4210a5cdc8ed68b216d5e42546e42a4720c
Renato Gonzales <renato@proxy.kodelab.xyz>
2024-03-12 13:12:38 -0500

    Agregado la llamada remota a la API de pronetheus

---
+++ README.md
@@ -0,0 +1,10 @@
+# PROMETHEUS CUSTOM EXPORTER PYTHON
+
+He creado este proyecto para apoyar a un amigo en su trabajo.
+
+Los siguientes directorios tienen la siguiente informacion
+
+- prometheus: configuracion de prometheus y definicion de alertas.
+- mock_files(2): archivos de prueba con valores distintos para probar la validacion de hash.
+- config: archivo de configuracion del exporter.
+- alertmanager: configuracion de alertmanager.
--- app.py
+++ app.py
@@ -2,8 +2,35 @@
 
 import os
 import time
+import yaml
+import requests
+
 from prometheus_client import start_http_server, Gauge, Enum
-# import requests
+
+class PrometheusApiClient:
+
+    def __init__(self, url:str) -> None:
+        self.url = f"{url}/api/v1/query?"
+
+    def custom_query_labels(self, query):
+        res = requests.get(f'{self.url}query={query}')
+        json = res.json()
+        if json["status"] == "success":
+            return map(self.__get_labels, json["data"]["result"])
+        else:
+            return []
+        
+    def __get_labels(self, metric):
+        return metric['metric']
+
+
+class Config:
+    __config = yaml.safe_load(open('/config/config.yaml','r'))
+    role = __config['role']
+    scan_paths = __config['scan_paths']
+    prometheus_url = __config['prometheus']['url'] if role == 'slave' else ""
+    prometheus_ssl = __config['prometheus']['disable_ssl'] if role == 'slave' else True
+
 
 class AppMetrics:
     """
@@ -11,43 +38,63 @@ class AppMetrics:
     application metrics into Prometheus metrics.
     """
 
-    def __init__(self, app_port=80, polling_interval_seconds=5):
-        self.app_port = app_port
+    def __init__(self, polling_interval_seconds=5):
         self.polling_interval_seconds = polling_interval_seconds
 
         # Prometheus metrics to collect
-        self.current_requests = Gauge("app_requests_current", "Current requests")
-        self.pending_requests = Gauge("app_requests_pending", "Pending requests")
-        self.file_detection = Gauge("file_exists","File exists",['filename','hostname'])
         self.total_uptime = Gauge("app_uptime", "Uptime")
+        self.file_sha1sum = Gauge("sha1sum_file","Original File",['filename','sha1sum','hostname','original'])
+        self.match_sha1sum = Gauge("sha1sum_match", "Remote File Match", ['filename','hostname'])
         self.health = Enum("app_health", "Health", states=["healthy", "unhealthy"])
+        self.prom_client = PrometheusApiClient(url=Config.prometheus_url) if Config.role == 'slave' else None
 
     def run_metrics_loop(self):
         """Metrics fetching loop"""
 
         while True:
-            self.check_files()
+            self.load_shasum()
             time.sleep(self.polling_interval_seconds)
+    
+    def __check_item_on_dict(self, lst, filename):
+        if lst['filename'] == filename:
+            # print(lst['filename'])
+            return lst['sha1sum']
 
-    def check_files(self):
-        file = '/Users/rgonzs/typeractive_case.stl'
-        cmd = os.popen("ls -l %s | awk '{ print $9 }'" % file,'r',1)
-        hostname = os.popen("hostname").read().strip()
-        metric = cmd.read().strip()
-        if metric:
-            self.file_detection.labels(file, hostname).set(1)
-        else:
-            self.file_detection.labels(file, hostname).set(0)
+    def load_shasum(self):
+        for dir in Config.scan_paths:
+            files = os.listdir(dir)
+            for file in files:
+                cmd = os.popen("sha1sum %s/%s | awk '{print $1}'" % (dir, file), 'r', 1)
+                hostname = os.popen("hostname").read().strip()
+                sha1sum = cmd.read().strip()
+                if Config.role == "master":
+                    if sha1sum:
+                        self.file_sha1sum.labels(file, sha1sum, hostname, 1).set(1)
+                    else:
+                        self.file_sha1sum.labels( '', '', '', 0).set(0)
+                if Config.role == "slave":
+                    original = self.prom_client.custom_query_labels('sha1sum_file{original="1"}')
+                    sha1sum_original = ""
+                    for item in original:
+                        sum = self.__check_item_on_dict(item, file)
+                        if sum:
+                            sha1sum_original = sum
+                        
+                    print({'sha1sum':sha1sum, 'sha1sum_original':sha1sum_original,'file': file})
+                    match = True if sha1sum_original == sha1sum else False
+                    if match:
+                        self.match_sha1sum.labels(file, hostname).set(1)
+                    else:
+                        self.match_sha1sum.labels(file, hostname).set(0)
+
 
 def main():
     """Main entry point"""
 
     polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "5"))
-    app_port = int(os.getenv("APP_PORT", "80"))
     exporter_port = int(os.getenv("EXPORTER_PORT", "9000"))
 
     app_metrics = AppMetrics(
-        app_port=app_port,
         polling_interval_seconds=polling_interval_seconds
     )
     start_http_server(exporter_port)
